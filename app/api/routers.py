185d62e8dd06d5d1606c3b40c16eefbbda4030cd
hornum <a.chere.pnin@mail.ru>
2026-03-07 21:34:24 +0300

    Add post route to make short url Add get short route and redirect

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,16 +1,49 @@
-from fastapi import APIRouter
+import datetime
+import string
+import secrets
 
-from app.models.links import MakeShortModel
+from fastapi import APIRouter, Query, HTTPException
+from sqlalchemy import select
+from starlette.responses import RedirectResponse
+
+from app.models.links import LinkModel
+from app.db.session import async_session_maker
+
+
+ALPHABET = string.ascii_letters + string.digits
+
+def generate_random_string():
+    return ''.join(secrets.choice(ALPHABET) for i in range(8))
 
 router = APIRouter(prefix="/links", tags=["Links"])
-
 @router.post('/')
-async def shorten_link(link: MakeShortModel):
-    pass
+async def shorten_link(link: str = Query(min_length=3)):
+    async with async_session_maker() as session:
+        new_link = LinkModel(
+            long_url=link,
+            short_url=generate_random_string(),
+            used_count=0,
+            created_at=datetime.datetime.now(),
+        )
+        session.add(new_link)
+        await session.commit()
+        return f'{new_link.short_url}'
 
 @router.get('/{short_link}')
 async def get_long_link(short_link: str):
-    pass
+    async with (async_session_maker() as session):
+        result = await session.execute(
+            select(LinkModel).where(LinkModel.short_url == short_link)
+        )
+        link = result.scalar_one_or_none()
+
+        if not link:
+            raise HTTPException(status_code=404, detail="Link not found")
+
+        link.used_count += 1
+        await session.commit()
+
+        return RedirectResponse(url=link.long_url)
 
 @router.get('/{short_link}/info')
 async def shorten_link(short_link: str):
